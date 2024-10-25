5f6d1bb30eb3301ecbde0fd83ebb1c43de36f55a
Vincent Gärtner <0@Z3l2A.de>
2024-10-25 11:08:14 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,153 @@
+{
+    "id": "47f0151f-9122-42f9-85a9-253d3b40f7c3",
+    "deletedDateTime": null,
+    "appId": "08e4b1a2-42ae-45a1-b281-f6ecf20f717a",
+    "applicationTemplateId": null,
+    "disabledByMicrosoftStatus": null,
+    "createdDateTime": "2024-10-23T08:04:29Z",
+    "displayName": "https://Z3l2A.github.io",
+    "description": null,
+    "groupMembershipClaims": null,
+    "identifierUris": [
+        "api://6810772"
+    ],
+    "isDeviceOnlyAuthSupported": null,
+    "isFallbackPublicClient": true,
+    "nativeAuthenticationApisEnabled": null,
+    "notes": "https://Z3l2A.github.io",
+    "publisherDomain": "Z3l2A.de",
+    "serviceManagementReference": null,
+    "signInAudience": "AzureADandPersonalMicrosoftAccount",
+    "tags": [],
+    "tokenEncryptionKeyId": null,
+    "samlMetadataUrl": null,
+    "defaultRedirectUri": null,
+    "certification": null,
+    "optionalClaims": null,
+    "requestSignatureVerification": null,
+    "addIns": [],
+    "api": {
+        "acceptMappedClaims": null,
+        "knownClientApplications": [],
+        "requestedAccessTokenVersion": 2,
+        "oauth2PermissionScopes": [
+            {
+                "adminConsentDescription": "*",
+                "adminConsentDisplayName": "*",
+                "id": "57eafa4a-78cb-4478-8054-4e1cd9226756",
+                "isEnabled": true,
+                "type": "User",
+                "userConsentDescription": "*",
+                "userConsentDisplayName": "*",
+                "value": "*"
+            }
+        ],
+        "preAuthorizedApplications": [
+            {
+                "appId": "08e4b1a2-42ae-45a1-b281-f6ecf20f717a",
+                "delegatedPermissionIds": [
+                    "57eafa4a-78cb-4478-8054-4e1cd9226756"
+                ]
+            }
+        ]
+    },
+    "appRoles": [],
+    "info": {
+        "logoUrl": null,
+        "marketingUrl": null,
+        "privacyStatementUrl": "https://z3l2a.github.io/",
+        "supportUrl": null,
+        "termsOfServiceUrl": "https://z3l2a.github.io/"
+    },
+    "keyCredentials": [
+        {
+            "customKeyIdentifier": "9CCD2CD89FA6C22C574F45E7806A6B3256536887",
+            "displayName": "*",
+            "endDateTime": "2038-01-15T17:59:59Z",
+            "key": null,
+            "keyId": "e347ecf9-0b05-4f67-ae06-d09181bf9e74",
+            "startDateTime": "2024-06-24T17:59:59Z",
+            "type": "AsymmetricX509Cert",
+            "usage": "Verify"
+        }
+    ],
+    "parentalControlSettings": {
+        "countriesBlockedForMinors": [],
+        "legalAgeGroupRule": "Allow"
+    },
+    "passwordCredentials": [
+        {
+            "customKeyIdentifier": null,
+            "displayName": "0",
+            "endDateTime": "2026-10-22T22:00:00Z",
+            "hint": "hvi",
+            "keyId": "dbef5b1d-efd2-4de8-bfe9-c402a56aca26",
+            "secretText": null,
+            "startDateTime": "2024-10-23T08:38:52Z"
+        }
+    ],
+    "publicClient": {
+        "redirectUris": []
+    },
+    "requiredResourceAccess": [
+        {
+            "resourceAppId": "00000003-0000-0000-c000-000000000000",
+            "resourceAccess": [
+                {
+                    "id": "e1fe6dd8-ba31-4d61-89e7-88639da4683d",
+                    "type": "Scope"
+                }
+            ]
+        },
+        {
+            "resourceAppId": "499b84ac-1321-427f-aa17-267ca6975798",
+            "resourceAccess": [
+                {
+                    "id": "ee69721e-6c3a-468f-a9ec-302d16a4c599",
+                    "type": "Scope"
+                },
+                {
+                    "id": "5f1d8cdf-acb3-47db-b79d-e0c6f18e262d",
+                    "type": "Scope"
+                }
+            ]
+        },
+        {
+            "resourceAppId": "797f4846-ba00-4fd7-ba43-dac1f8f63013",
+            "resourceAccess": [
+                {
+                    "id": "41094075-9dad-400e-a0bd-54e686782033",
+                    "type": "Scope"
+                }
+            ]
+        }
+    ],
+    "verifiedPublisher": {
+        "displayName": null,
+        "verifiedPublisherId": null,
+        "addedDateTime": null
+    },
+    "web": {
+        "homePageUrl": "https://Z3l2A.github.io",
+        "logoutUrl": null,
+        "redirectUris": [],
+        "implicitGrantSettings": {
+            "enableAccessTokenIssuance": true,
+            "enableIdTokenIssuance": true
+        },
+        "redirectUriSettings": []
+    },
+    "servicePrincipalLockConfiguration": {
+        "isEnabled": false,
+        "allProperties": true,
+        "credentialsWithUsageVerify": true,
+        "credentialsWithUsageSign": true,
+        "identifierUris": true,
+        "tokenEncryptionKeyId": true
+    },
+    "spa": {
+        "redirectUris": [
+            "https://Z3l2A.github.io"
+        ]
+    }
+}
